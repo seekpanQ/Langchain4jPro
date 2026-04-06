e9da83a58a4a7d7b5682da9a8f95f917ac3fd526
seekpan <lixin_qiu@163.com>
2026-04-06 17:11:23 +0800

    多模型调用测试

--- DemoLLM/src/main/java/com/test/config/LLMConfig.java
+++ DemoLLM/src/main/java/com/test/config/LLMConfig.java
@@ -14,13 +14,23 @@ public class LLMConfig {
 
      该方法执行后：拿到这个返回的对象后，你就可以用它来调用其方法，实现与 AI 模型的对话，
      */
-    @Bean
+    @Bean("qwen")
     public ChatModel chatModelQwen() {
 
         return OpenAiChatModel.builder()
                 .apiKey(System.getenv("qwenapikey"))// 从环境变量获取apikey
                 .modelName("qwen3-max")// 模型名
                 .baseUrl("https://dashscope.aliyuncs.com/compatible-mode/v1")// 调用地址
+                .build();
+    }
+
+    @Bean("deepseek")
+    public ChatModel chatModelDeepseek() {
+
+        return OpenAiChatModel.builder()
+                .apiKey(System.getenv("deepseekapikey"))// 从环境变量获取apikey
+                .modelName("deepseek-chat")// 模型名
+                .baseUrl("https://api.deepseek.com/v1")// 调用地址
                 .build();
     }
 }
--- DemoLLM/src/main/java/com/test/controller/DemoController.java
+++ DemoLLM/src/main/java/com/test/controller/DemoController.java
@@ -2,6 +2,7 @@ package com.test.controller;
 
 import dev.langchain4j.model.chat.ChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,17 +13,30 @@ public class DemoController {
 
     // 自动注入我们配置的大模型啊
     @Autowired
-    private ChatModel chatModel;
+    @Qualifier("qwen")
+    private ChatModel chatModelQwen;
 
-    @GetMapping(value = "/langchain4j/hello") // 请求路径
+    @Autowired
+    @Qualifier("deepseek")
+    private ChatModel chatModelDeepseek;
+
+    @GetMapping(value = "/langchain4j/qwen") // 请求路径
     @ResponseBody // 方法的返回值应该直接写入 HTTP 响应体中
     /*
     value = "question"：指定要从请求中获取名为 question 的参数
     defaultValue = "你是谁"：如果没有提供 question 参数，使用默认值 "你是谁"
     String question：将提取的参数值绑定到这个方法参数
      */
-    public String hello(@RequestParam(value = "question", defaultValue = "你是谁") String question) {
-        String result = chatModel.chat(question);
+    public String qwenCall(@RequestParam(value = "question", defaultValue = "你是谁") String question) {
+        String result = chatModelQwen.chat(question);
+        System.out.println("调用大模型回复: " + result);
+        return result;
+    }
+
+    @GetMapping(value = "/langchain4j/deepseek") // 请求路径
+    @ResponseBody // 方法的返回值应该直接写入 HTTP 响应体中
+    public String deepseekCall(@RequestParam(value = "question", defaultValue = "你是谁") String question) {
+        String result = chatModelDeepseek.chat(question);
         System.out.println("调用大模型回复: " + result);
         return result;
     }
